0023981e3f5200f3a4482feed3eb9d0a36c302b7
Github Ehei <ehei123@gmail.com>
2026-05-15 16:40:00 +0800

    step 5 20260515 Load DB data to template

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 APPLICATION_APPS = [
@@ -49,11 +50,14 @@ APPLICATION_APPS = [
     'listings.apps.ListingsConfig',
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+    'debug_toolbar',
+]
 
 INSTALLED_APPS = DJANGO_APPS + APPLICATION_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -140,3 +144,10 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR,'config/static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'

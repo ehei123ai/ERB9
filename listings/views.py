f0bdd7cba93041022b0a8c38b11aaf3675f43dfe
Github Ehei <ehei123@gmail.com>
2026-05-18 14:34:26 +0800

    step 7: listing template done

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Listing
 from django.core.paginator import Paginator
@@ -13,4 +13,6 @@ def listings(request):
     return render(request, "listings/listings.html", context)
 
 def listing(request, listing_id):
-    return render(request, "listings/listing.html")
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {"listing": listing}
+    return render(request, "listings/listing.html", context)

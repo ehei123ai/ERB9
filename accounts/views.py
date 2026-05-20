5b4cc52025f509478b3d410984890b5c91541763
Github Ehei <ehei123@gmail.com>
2026-05-20 15:15:58 +0800

    step 11: accounts APP done

--- accounts/views.py
+++ accounts/views.py
@@ -45,14 +45,16 @@ def login(request):
         if user is not None:
             auth.login(request, user)
             messages.success(request, "You are now logged in")
-            return redirect( "listings:index" )
+            return redirect( "accounts:dashboard" )
         else:
             messages.error(request, "Invalid credentials")
             return redirect( "accounts:login" )
     return render(request, 'accounts/login.html')
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    if request.method == "POST":
+        auth.logout(request)
+        return redirect( 'pages:index' )
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')

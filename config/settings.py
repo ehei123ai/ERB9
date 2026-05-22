89377a125ef319c3091e8e36188d93f23d6f48c7
Github Ehei <ehei123@gmail.com>
2026-05-22 16:20:58 +0800

    step 13: taggit

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ APPLICATION_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
-    'debug_toolbar', "widget_tweaks"
+    'debug_toolbar', "widget_tweaks", "taggit"
 ]
 
 INSTALLED_APPS = DJANGO_APPS + APPLICATION_APPS + THIRD_PARTY_APPS
